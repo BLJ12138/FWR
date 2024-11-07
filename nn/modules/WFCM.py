8d3178436de93feb77e3a2d690aa4372d714d56c
BLJ12138 <15166191278@163.com>
2024-11-07 20:27:25 +0800

    Update WFCM.py

--- nn/modules/WFCM.py
+++ nn/modules/WFCM.py
@@ -2,15 +2,4 @@ import torch
 from torch import nn
 
 class WFCM(nn.Module):
-    def __init__(self, dimension=1):
-        super(WFCM, self).__init__()
-        self.d = dimension
-        self.w = nn.Parameter(torch.ones(2, dtype=torch.float32), requires_grad=True)
-        self.epsilon = 0.0001
-
-    def forward(self, x):
-        w = self.w
-        weight = w / (torch.sum(w, dim=0) + self.epsilon)  # 将权重进行归一化
-        # Fast normalized fusion
-        x = [weight[0] * x[0], weight[1] * x[1]]
-        return torch.cat(x, self.d)
+This section will be made public upon acceptance of the paper.
